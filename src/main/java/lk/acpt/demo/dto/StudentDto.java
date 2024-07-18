5ce1acbb8119d9ea1283bae3cec7d0527f4d6dc1
Shanilka <shanilka799@gmail.com>
2024-07-18 12:02:54 +0530

    add student dto and service class

---
+++ src/main/java/lk/acpt/demo/dto/StudentDto.java
@@ -0,0 +1,8 @@
+package lk.acpt.demo.dto;
+
+public class StudentDto {
+    private Integer id;
+    private String name;
+    private String address;
+    private double marks;
+}
---
+++ src/main/java/lk/acpt/demo/service/StudentService.java
@@ -0,0 +1,4 @@
+package lk.acpt.demo.service;
+
+public class StudentService {
+}
